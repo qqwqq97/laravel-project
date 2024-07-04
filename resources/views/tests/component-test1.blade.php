c5311a42ef015333049965deb60d841ca6560826
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2024-07-04 22:32:46 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,5 +5,6 @@
 コンポーネントテスト1
     <x-tests.card title="タイトル1" content="本文1" :message="$message"/>
     <x-tests.card title="タイトル1" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 
 </x-tests.app>
