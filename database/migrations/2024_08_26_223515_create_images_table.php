08f27b3806c3ce4b4bbecde4c67410b8b3797df1
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2024-08-26 22:44:21 +0900

    image初期設定

--- database/migrations/2024_08_26_223515_create_images_table.php
+++ database/migrations/2024_08_26_223515_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateShopsTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateShopsTable extends Migration
      */
     public function up()
     {
-        Schema::create('shops', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->foreignId('owner_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->string('name');
-            $table->text('information');
             $table->string('filename');
-            $table->boolean('is_selling');
+            $table->string('title')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateShopsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('shops');
+        Schema::dropIfExists('images');
     }
 }
