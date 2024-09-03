be877c95f27d4dfe427b4bd1fc0c40facd2ed7b9
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2024-09-03 21:57:10 +0900

    product初期設定

--- resources/views/owner/images/create.blade.php
+++ resources/views/owner/images/create.blade.php
@@ -17,8 +17,9 @@
                                 <div class="relative">
                                     <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                                     <input type="file" id="image" name="files[][image]" multiple accept="image/png,image/jpeg,image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                </div>
+                                </div>  
                             </div>   
+                            <!-- files[][image]：複数のファイルを登録するため -->
                             <div class="p-2 w-full flex justify-around mt-4">
                                 <button type="button" onclick="location.href='{{ route('owner.images.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                 <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
