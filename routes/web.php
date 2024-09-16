0b985a4e7ffe22281811dc0c1b0c6e6aef86563c
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2024-09-16 16:06:46 +0900

    stripe controller

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ middleware('auth:users')->group(function(){
     Route::get('/', [CartController::class,'index'])->name('cart.index');
     Route::post('add', [CartController::class,'add'])->name('cart.add');
     Route::post('delete/{item}', [CartController::class,'delete'])->name('cart.delete');
+    Route::get('checkout', [CartController::class,'checkout'])->name('cart.checkout');
 
 });    
 
